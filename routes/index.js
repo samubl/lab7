a6ac2c466317529e8e2b0309868b581b00add9e8
Samuel Blake <samuelblake97@gmail.com>
2019-02-21 13:25:46 -0800

    Set up Google Analytics
    Tracking differences between viewAlt and regular page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var projects = require('../projects.json');
-
 /*
  * GET home page.
  */
 
 exports.view = function(request, response){
-  	response.render('index', projects);
+	projects["viewAlt"] = false;
+	response.render('index', projects,);
+};
+
+exports.viewAlt = function(request, response){
+	projects["viewAlt"] = true;
+	response.render('index', projects);
 };
